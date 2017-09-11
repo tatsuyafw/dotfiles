c13edd5386ba73e222898f0d72a176b079c8be94
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2017-09-12 00:13:34 +0900

    Fix key binding of helm-buffers-list

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -321,8 +321,8 @@
   (define-key global-map (kbd "M-x")     'helm-M-x)
   (define-key global-map (kbd "C-x C-f") 'helm-find-files)
   (define-key global-map (kbd "C-x C-r") 'helm-recentf)
+  (define-key global-map (kbd "C-x C-b") 'helm-buffers-list)
   (define-key global-map (kbd "C-c i")   'helm-imenu)
-  (define-key global-map (kbd "C-x b")   'helm-buffers-list)
 
   (define-key helm-map (kbd "C-h") 'delete-backward-char)
   (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
