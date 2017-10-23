fc34fb99286bf3d229ba145969237e1fa0233784
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2017-10-24 00:14:47 +0900

    Manage tmux powerline status configs

--- bin/init.sh
+++ bin/init.sh
@@ -2,6 +2,9 @@
 
 basedir=`dirname $0`/../..
 
+mkdir -p $HOME/.config
+cp -r .config/powerline $HOME/.config/
+
 FILE_LIST=(.eslintrc.json .emacs.d .pryrc .pylintrc .rspec .screenrc .tmux.conf .vimrc .vim .zshrc)
 
 : [Info] Set synblic link
